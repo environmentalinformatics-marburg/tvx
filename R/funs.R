a800a9d08b140f368403e067a6419697ad1564a7
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-05-12 18:24:51 +0200

    Added function for the computation of standard error.

--- R/funs.R
+++ R/funs.R
@@ -1,4 +1,4 @@
-#' Compute Root Mean Square error
+#' Compute Root Mean Square Error
 #' 
 rmse <- function(x, y) {
   sqrt(mean((y - x)^2, na.rm = TRUE))
@@ -10,4 +10,10 @@ movingWindow <- function(width = 7L, height = width) {
   mat <- matrix(1, nrow = height, ncol = width)
   mat[ceiling(height/2), ceiling(width/2)] <- 0
   mat
+}
+
+#' Compute Standard Error
+#' 
+stde <- function(x) {
+  sd(x, na.rm = TRUE) / sqrt(sum(!is.na(x)))
 }
