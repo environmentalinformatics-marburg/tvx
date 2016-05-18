fd72e55c14cec099a68cf47da1b1f609f5ba07c8
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-05-18 17:04:44 +0200

    RMSE and RMSE.se are available from Rsenal::regressionStats().

--- R/funs.R
+++ R/funs.R
@@ -1,19 +1,7 @@
-#' Compute Root Mean Square Error
-#' 
-rmse <- function(x, y) {
-  sqrt(mean((y - x)^2, na.rm = TRUE))
-}
-
 #' Setup Moving Window
 #' 
 movingWindow <- function(width = 7L, height = width) {
   mat <- matrix(1, nrow = height, ncol = width)
   mat[ceiling(height/2), ceiling(width/2)] <- 0
   mat
-}
-
-#' Compute Standard Error
-#' 
-stde <- function(x) {
-  sd(x, na.rm = TRUE) / sqrt(sum(!is.na(x)))
 }
