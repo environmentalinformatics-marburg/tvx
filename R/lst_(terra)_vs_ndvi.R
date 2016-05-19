2db05b6d32f5e1351fcb83e55a7194a92a3a599d
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-05-19 17:24:13 +0200

    Outsourced visualization to R/figure02.R.

--- R/lst_(terra)_vs_ndvi.R
+++ R/lst_(terra)_vs_ndvi.R
@@ -4,7 +4,7 @@
 rm(list = ls(all = TRUE))
 
 ## load packages
-lib <- c("dplyr", "foreach", "doParallel", "MODIS", "raster", "rgdal", 
+lib <- c("dplyr", "foreach", "doParallel", "MODIS", "raster", "rgdal", "grid",
          "hydroGOF", "lubridate", "caret", "latticeExtra", "reshape2", "Rsenal")
 Orcs::loadPkgs(lib)
 
@@ -111,93 +111,95 @@ dat_plots$habitat[dat_plots$plotID == "mwh0"] <- "fpd"
 ### select training plots -----
 
 ## loop over plots
-# dat_select <- foreach(h = as.character(unique(dat_plots$plotID)), 
-#                .packages = lib, .combine = "rbind") %dopar% {
-# 
-#   ## extract daily maximum temperature
-#   dat_plots %>%
-#     filter(plotID == h & !is.na(Ta_200)) %>%
-#     group_by(Date = as.Date(datetime)) %>%
-#     filter(length(Ta_200) == 24) %>%
-#     summarise(Ta_200 = max(Ta_200, na.rm = TRUE)) %>%
-#     data.frame() -> dat_sub
-#   
-#   ## synchronise with modis time steps, i.e. aggregate 8-day mean maximum values 
-#   dat_agg <- foreach(i = unique(lubridate::year(dat_sub$Date)), 
-#                      .combine = "rbind") %do% {
-# 
-#     # merge available dates with continuous daily time series
-#     dts0 <- seq(as.Date(paste0(i, "-01-01")), as.Date(paste0(i, "-12-31")), 1)
-#     mrg <- merge(data.frame(Date = dts0), dat_sub, all.x = TRUE)
-#     
-#     # aggregate 8-day intervals
-#     mrg$Cuts <- cut(mrg$Date, "8 days")
-#     mrg <- mrg[complete.cases(mrg), ]
-#     
-#     mrg %>% 
-#       group_by(Cuts) %>%
-#       summarise(Ta_200 = round(mean(Ta_200, na.rm = TRUE), 2)) %>%
-#       data.frame()
-#   }
-#   
-#   ## extract intercept and slope values
-#   dts_lst <- MODIS::extractDate(names(rst_lst))$inputLayerDates
-#   avl <- dts_lst %in% strftime(dat_agg$Cuts, "%Y%j")
-#   
-#   if (all(!avl) | sum(avl) == 1) {
-#     return(data.frame(PlotID = h, NDVImax = NA, rsq = NA, rmse = NA, bias = NA,
-#                       n = ifelse(sum(avl) == 1, 1, NA)))
-#   }
-#   
-#   metrics <- foreach(i = list(mat_intercept, mat_slope), 
-#                      j = list(rst_intercept, rst_slope)) %do% {
-# 
-#     mat <- i[, avl]
-#     rst <- j[[which(avl)]]
-#                        
-#     cell_id <- cellFromXY(j, plots[plots@data$PlotID == h, ])
-#     val <- mat[cell_id, ]
-#                        
-#     if (all(is.na(val))) {
-#       data.frame(Date = MODIS::extractDate(names(rst))$inputLayerDates, 
-#                  rep(NA, length(MODIS::extractDate(names(rst))$inputLayerDates)))
-#     } else {
-#       data.frame(Date = MODIS::extractDate(names(rst))$inputLayerDates, val)
-#     }
-#   }
-#   
-#   metrics <- Reduce(function(...) merge(..., by = "Date", all = TRUE), metrics)
-#   names(metrics)[2:3] <- c("Intercept", "Slope")
-#   
-#   if (all(is.na(metrics$Intercept)) | all(is.na(metrics$Slope))) {
-#     return(data.frame(PlotID = h, NDVImax = NA, rsq = NA, rmse = NA, bias = NA, n = NA))
-#   }
-#   
-#   ## loop over sequence of ndvi_max values
-#   dat_agg$Date <- strftime(dat_agg$Cuts, "%Y%j")
-#   dat_mrg <- merge(dat_agg, metrics, all.y = TRUE)
-#   
-#   n <- sum(complete.cases(dat_mrg))
-#   
-#   ta_obs <- dat_mrg$Ta_200
-#   
-#   stats <- foreach(i = seq(.2, 1, .01), .combine = "rbind") %do% {
-#     
-#     # estimate temperature
-#     ta_prd <- (dat_mrg$Slope * i + dat_mrg$Intercept)
-#     
-#     # calculate r-squared, rmse, and bias
-#     data.frame(PlotID = h, NDVImax = i, 
-#                rsq = summary(lm(ta_prd ~ ta_obs))$r.squared, 
-#                rmse = rmse(ta_obs, ta_prd), 
-#                bias = pbias(ta_prd, ta_obs), n = n)
-#   }
-#   
-#   # parallel::stopCluster(cl2)
-#   stats[which.min(stats$rmse), ]
-# }
-# 
-# saveRDS(out, file = "data/results/ndvimax_rmse_7by7_terra.rds")
+dat_select <- foreach(h = as.character(unique(dat_plots$plotID)),
+               .packages = lib, .combine = "rbind") %dopar% {
+
+  ## extract daily maximum temperature
+  dat_plots %>%
+    filter(plotID == h & !is.na(Ta_200)) %>%
+    group_by(Date = as.Date(datetime)) %>%
+    filter(length(Ta_200) == 24) %>%
+    summarise(Ta_200 = max(Ta_200, na.rm = TRUE)) %>%
+    data.frame() -> dat_sub
+
+  ## synchronise with modis time steps, i.e. aggregate 8-day mean maximum values
+  dat_agg <- foreach(i = unique(lubridate::year(dat_sub$Date)),
+                     .combine = "rbind") %do% {
+
+    # merge available dates with continuous daily time series
+    dts0 <- seq(as.Date(paste0(i, "-01-01")), as.Date(paste0(i, "-12-31")), 1)
+    mrg <- merge(data.frame(Date = dts0), dat_sub, all.x = TRUE)
+
+    # aggregate 8-day intervals
+    mrg$Cuts <- cut(mrg$Date, "8 days")
+    mrg <- mrg[complete.cases(mrg), ]
+
+    mrg %>%
+      group_by(Cuts) %>%
+      summarise(Ta_200 = round(mean(Ta_200, na.rm = TRUE), 2)) %>%
+      data.frame()
+  }
+
+  ## extract intercept and slope values
+  dts_lst <- MODIS::extractDate(names(rst_lst))$inputLayerDates
+  avl <- dts_lst %in% strftime(dat_agg$Cuts, "%Y%j")
+
+  if (all(!avl) | sum(avl) == 1) {
+    return(data.frame(PlotID = h, NDVImax = NA, rsq = NA, rmse = NA, bias = NA,
+                      n = ifelse(sum(avl) == 1, 1, NA)))
+  }
+
+  metrics <- foreach(i = list(mat_intercept, mat_slope),
+                     j = list(rst_intercept, rst_slope)) %do% {
+
+    mat <- i[, avl]
+    rst <- j[[which(avl)]]
+
+    cell_id <- cellFromXY(j, plots[plots@data$PlotID == h, ])
+    val <- mat[cell_id, ]
+
+    if (all(is.na(val))) {
+      data.frame(Date = MODIS::extractDate(names(rst))$inputLayerDates,
+                 rep(NA, length(MODIS::extractDate(names(rst))$inputLayerDates)))
+    } else {
+      data.frame(Date = MODIS::extractDate(names(rst))$inputLayerDates, val)
+    }
+  }
+
+  metrics <- Reduce(function(...) merge(..., by = "Date", all = TRUE), metrics)
+  names(metrics)[2:3] <- c("Intercept", "Slope")
+
+  if (all(is.na(metrics$Intercept)) | all(is.na(metrics$Slope))) {
+    return(data.frame(PlotID = h, NDVImax = NA, rsq = NA, rmse = NA, bias = NA, n = NA))
+  }
+
+  ## loop over sequence of ndvi_max values
+  dat_agg$Date <- strftime(dat_agg$Cuts, "%Y%j")
+  dat_mrg <- merge(dat_agg, metrics, all.y = TRUE)
+
+  n <- sum(complete.cases(dat_mrg))
+
+  ta_obs <- dat_mrg$Ta_200
+
+  stats <- foreach(i = seq(.2, 1, .01), .combine = "rbind") %do% {
+
+    # estimate temperature
+    ta_prd <- (dat_mrg$Slope * i + dat_mrg$Intercept)
+
+    # calculate r-squared, rmse, and bias
+    data.frame(PlotID = h, NDVImax = i,
+               rsq = summary(lm(ta_prd ~ ta_obs))$r.squared,
+               rmse = rmse(ta_obs, ta_prd),
+               bias = pbias(ta_prd, ta_obs), n = n)
+  }
+
+  # parallel::stopCluster(cl2)
+  stats[which.min(stats$rmse), ]
+}
+
+dat_select <- dat_select[complete.cases(dat_select), ]
+
+saveRDS(dat_select, file = "data/results/ndvimax_rmse_7by7_terra.rds")
 dat_select <- readRDS("data/results/ndvimax_rmse_7by7_terra.rds")
 
 ## extract habitat information
@@ -351,72 +353,6 @@ dat_training$habitat <- factor(dat_training$habitat,
                                levels = c("mai", "sav", "cof", "hom", "gra", 
                                           "flm", "fod", "foc", "fpo", "fpd", 
                                           "fer", "fed", "hel"))
-
-
-### display training data -----
-
-prm <- c("habitat", "TrainRsq", "TrainRMSE", "TrainRMSEse", 
-         "Rsq", "RMSE", "RMSEse")
-training_stats_mlt <- melt(dat_training[, prm])
-
-xlim_rsq <- c(0, max(max(dat_training$TrainRsq), max(dat_training$Rsq)))
-xlim_rmse <- c(0, max(max(dat_training$TrainRMSE + dat_training$TrainRMSEse), 
-                      max(dat_training$RMSE + dat_training$RMSEse)))
-
-p_trn <- dotplot(habitat ~ value | variable, xlim = list(xlim_rsq, xlim_rmse), 
-                 data = subset(training_stats_mlt, 
-                               variable %in% c("TrainRsq", "TrainRMSE")),
-                 cex = 1.2, ylab = "Habitat", 
-                 scales = list(x = list(relation = "free")), 
-                 panel = function(x, y, ...) {
-                   panel.dotplot(x, y, col.line = "grey90")
-                   for (i in 1:length(levels(dat_training$habitat))) {
-                     hab <- levels(dat_training$habitat)[i]
-                     tmp <- subset(dat_training, habitat == hab)
-                     
-                     if (panel.number() == 2) {
-                       xmn <- tmp$TrainRMSE - tmp$TrainRMSEse
-                       xmn <- ifelse(xmn < 0, 0, xmn)
-                       
-                       xmx <- tmp$TrainRMSE + tmp$TrainRMSEse
-                       
-                       panel.lines(x = c(xmn, xmx), y = i, col = "black", 
-                                   lwd = 1.2)
-                       panel.dotplot(x = c(xmn, xmx), y = i, pch = "|", cex = 1.1,
-                                     col = "black", col.line = "transparent")
-                     }
-                   }
-                   
-                   panel.dotplot(x, y, col = "black", col.line = "transparent", 
-                                 ...)
-                 })
-
-p_tst <- dotplot(habitat ~ value | variable, xlim = list(xlim_rsq, xlim_rmse), 
-                 data = subset(training_stats_mlt, variable %in% c("Rsq", "RMSE")),
-                 cex = 1.2, ylab = "Habitat", 
-                 scales = list(x = list(relation = "free")), 
-                 panel = function(x, y, ...) {
-                   panel.dotplot(x, y, col.line = "grey90")
-                   for (i in 1:length(levels(dat_training$habitat))) {
-                     hab <- levels(dat_training$habitat)[i]
-                     tmp <- subset(dat_training, habitat == hab)
-                     
-                     if (panel.number() == 2) {
-                       xmn <- tmp$RMSE - tmp$RMSEse
-                       xmn <- ifelse(xmn < 0, 0, xmn)
-                       
-                       xmx <- tmp$RMSE + tmp$RMSEse
-                       
-                       panel.lines(x = c(xmn, xmx), y = i, col = "black", 
-                                   lwd = 1.2)
-                       panel.dotplot(x = c(xmn, xmx), y = i, pch = "|", cex = 1.1,
-                                     col = "black", col.line = "transparent")
-                     }
-                   }
-                   
-                   panel.dotplot(x, y, col = "black", col.line = "transparent", 
-                                 ...)
-                 })
 
 
 ### evaluate approach at habitat scale -----
@@ -609,76 +545,15 @@ prediction_stats$habitat <- factor(prediction_stats$habitat,
 #   }
 # }
 #   
-# saveRDS(prediction_stats_plot, "data/results/stats_plot_inter_7by7_terra.rds")
-prediction_stats_plot <- readRDS("data/results/stats_plot_inter_7by7_terra.rds")
-
-prediction_stats_plot <- merge(prediction_stats_plot, 
-                               dat_test[, c("PlotID", "habitat")], 
-                               by = "PlotID", all.x = TRUE)
-
-## reorder factor levels
-prediction_stats_plot$habitat <- factor(prediction_stats_plot$habitat, 
-                                   levels = c("mai", "sav", "cof", "hom", "gra", 
-                                              "flm", "fod", "foc", "fpo", "fpd", 
-                                              "fer", "fed", "hel"))
-
-
-### display test data -----
-
-prediction_stats_mlt <- melt(prediction_stats[, c("habitat", "rsq", "rmse", "rmse.se")])
-# prediction_stats_plot_mlt <- 
-#   melt(prediction_stats_plot[, c("habitat", "rsq", "rmse")])
-
-xlim_rsq <- c(0, max(prediction_stats$rsq))
-xlim_rmse <- c(0, max(prediction_stats$rmse + prediction_stats$rmse.se))
-
-p_tst <- dotplot(habitat ~ value | variable, xlim = list(xlim_rsq, xlim_rmse), 
-                 data = subset(prediction_stats_mlt, variable %in% c("rsq", "rmse")),
-                 cex = 1.2, col = "black", xlab = "RMSE", ylab = "Habitat", 
-                 scales = list(x = list(relation = "free")), 
-                 panel = function(x, y, ...) {
-                   panel.dotplot(x, y, col.line = "grey90")
-                   for (i in 1:length(levels(prediction_stats$habitat))) {
-                     hab <- levels(prediction_stats$habitat)[i]
-                     tmp <- subset(prediction_stats, habitat == hab)
-                     
-                     if (panel.number() == 2) {
-                       xmn <- tmp$rmse - tmp$rmse.se
-                       xmn <- ifelse(xmn < 0, 0, xmn)
-                       
-                       xmx <- tmp$rmse + tmp$rmse.se
-                       
-                       panel.lines(x = c(xmn, xmx), y = i, col = "black", 
-                                   lwd = 1.2)
-                       panel.dotplot(x = c(xmn, xmx), y = i, pch = "|", cex = 1.1,
-                                     col = "black", col.line = "transparent")
-                     }
-                   }
-
-                   panel.dotplot(x, y, col.line = "transparent", ...)
-                 })
-
-
-# p1 <- dotplot(habitat ~ value | variable, data = prediction_stats_plot_mlt, 
-#               pch = 4, col.line = "grey90", xlab = "",
-#               scales = list(x = list(relation = "free")),
-#               panel = function(x, ...) {
-#                 panel.dotplot(x, col = ifelse(panel.number() == 1, "transparent", "grey"), ...)
-#                 
-#                 for (i in 1:length(levels(prediction_stats_plot$habitat))) {
-#                   hab <- levels(prediction_stats_plot$habitat)[i]
-#                   tmp <- subset(prediction_stats_plot, habitat == hab)
-#                   
-#                   if (panel.number() == 2) {
-#                   #   panel.lines(x = c(min(tmp$rsq, na.rm = TRUE),
-#                   #                     max(tmp$rsq, na.rm = TRUE)),
-#                   #               y = i, col = "grey30")
-#                   # } else {
-#                     panel.lines(x = c(min(tmp$rmse, na.rm = TRUE), 
-#                                       max(tmp$rmse, na.rm = TRUE)), 
-#                                 y = i, col = "grey30")
-#                   }
-#                 }
-#               })
+# # saveRDS(prediction_stats_plot, "data/results/stats_plot_inter_7by7_terra.rds")
+# prediction_stats_plot <- readRDS("data/results/stats_plot_inter_7by7_terra.rds")
 # 
-# p1 + as.layer(p2)
+# prediction_stats_plot <- merge(prediction_stats_plot, 
+#                                dat_test[, c("PlotID", "habitat")], 
+#                                by = "PlotID", all.x = TRUE)
+# 
+# ## reorder factor levels
+# prediction_stats_plot$habitat <- factor(prediction_stats_plot$habitat, 
+#                                    levels = c("mai", "sav", "cof", "hom", "gra", 
+#                                               "flm", "fod", "foc", "fpo", "fpd", 
+#                                               "fer", "fed", "hel"))
