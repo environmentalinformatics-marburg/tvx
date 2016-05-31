e08fbfb952af3e33606ddbd68087c4ab53ad59d4
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-05-31 13:20:53 +0200

    Removed training plots from table 1; inserted n.

--- R/table01.R
+++ R/table01.R
@@ -14,14 +14,14 @@ source("R/funs.R")
 ### ndvi_max -----
 
 
-dat_all <- readRDS("data/results/stats_intra_7by7_terra.rds")[, 1:2]
+dat_all <- readRDS("data/results/stats_intra_7by7_terra.rds")[, 1:3]
 dat_all$NDVImax <- round(dat_all$NDVImax, 2)
-dat_all$"Training plots" <- c("cof1,3", "fed3,4", "fer0", "flm1,4", "foc1,2", 
-                              "fod3,5", "fpd3,5", "fpo4,5", "gra4,5", 
-                              "hel4,5", "hom4,5", "mai3,4", "sav1,3")
-
-dat_all <- dat_all[, c(1, 3, 2)]
-rownames(dat_all) <- NULL
+# dat_all$"Training plots" <- c("cof1,3", "fed3,4", "fer0", "flm1,4", "foc1,2", 
+#                               "fod3,5", "fpd3,5", "fpo4,5", "gra4,5", 
+#                               "hel4,5", "hom4,5", "mai3,4", "sav1,3")
+# 
+# dat_all <- dat_all[, c(1, 3, 2)]
+# rownames(dat_all) <- NULL
 names(dat_all)[1] <- "Habitat"
 
 
@@ -38,7 +38,7 @@ dst <- c("fed", "fpd", "fod", "gra", "hom", "cof", "mai")
 
 dat_all$Disturbance <- "No"
 dat_all$Disturbance[dat_all$Habitat %in% dst] <- "Yes"
-dat_all <- dat_all[, c("Habitat", "Disturbance", "Elevation", "Training plots", 
+dat_all <- dat_all[, c("Habitat", "Disturbance", "Elevation", "n", 
                        "NDVImax")]
 
 
