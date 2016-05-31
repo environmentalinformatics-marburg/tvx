dfa760ac6facb296f2c2762adc179f0747e7d8d9
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-05-31 13:20:22 +0200

    Enabled parallel features.

--- R/weighted_mean.R
+++ R/weighted_mean.R
@@ -3,15 +3,16 @@ library(rgeos)
 ## polygons: ndvi
 rst_tmp <- rst_ndvi[[1]]
 rst_tmp[is.na(rst_tmp[])] <- 0
-spy_ndvi <- as(rst_tmp, "SpatialPolygons")
+spy_ndvi <- rasterToPolygons(rst_tmp)
 
 ## polygons: lst
 rst_tmp <- rst_lst[[1]]
 rst_tmp[is.na(rst_tmp[])] <- 0
-spy_lst <- as(rst_tmp, "SpatialPolygons")
+spy_lst <- rasterToPolygons(rst_tmp)
 
 ## calculate proportionate area contributions of modis cmg cell per gimms cell
-lst_areas <- lapply(1:length(spy_lst), function(i) {
+lst_areas <- foreach(i = 1:length(spy_lst), 
+                     .packages = c("raster", "rgeos")) %dopar% {
   cutset <- gIntersects(spy_ndvi, spy_lst[i, ], byid = TRUE)
   cutset <- as(cutset, "logical")
   spy_ndvi_sub <- spy_ndvi[cutset, ]
@@ -22,4 +23,4 @@ lst_areas <- lapply(1:length(spy_lst), function(i) {
   })
   
   data.frame(cell = which(cutset), area = spy_ndvi_areas)
-})
+}
